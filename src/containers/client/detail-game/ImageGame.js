7829ea2eb0498d584a3ef28aefe13c502bc4612c
luan <xoaygio2001@gmail.com>
2024-06-09 09:52:51 +0700

    update

--- src/containers/client/detail-game/ImageGame.js
+++ src/containers/client/detail-game/ImageGame.js
@@ -32,7 +32,6 @@ class ImageGame extends Component {
                         style={{ backgroundImage: `url(${imgFather})` }}
                     />
                 }
-
             </div>
         )
     }
